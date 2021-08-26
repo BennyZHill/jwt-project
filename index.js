d2ec7e74acc961b9f5fee109169bfd77bfb7b68b
BennyZHill <ben.h@builtbypixel.com>
2021-08-26 11:49:18 +0100

    htttp

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+
+const { connection } = require("./db");
+const User = require("./models/user");
 const errorRouter = require("./routes/error");
+const userRouter = require("./routes/user");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ msg: "process.env" });
-});
-app.use("*", errorRouter);
-app.listen(process.env.HHTP_PORT || 5000, () => {
-  console.log("HTTP SERVER STARTED");
+app.use(express.json());
+
+app.use("/user", userRouter);
+app.use("*", errorRouter); // Use error router here
+
+app.listen(process.env.HTTP_PORT || 5000, async () => {
+  connection.authenticate();
+  await User.sync({ alter: true }); // This creates/updates tables
+  console.log("HTTP Server Started");
 });
