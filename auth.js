c74be0453db69310c5103af20cd24bb9e89abb96
BennyZHill <ben.h@builtbypixel.com>
2021-08-27 09:08:17 +0100

    SPELLING MISTAKE

--- auth.js
+++ auth.js
@@ -1,19 +1,22 @@
 const bcrypt = require("bcrypt");
-const ExctractJWT = require("passport-jwt").ExtractJwt;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
 const JWTStrategy = require("passport-jwt").Strategy;
 const LocalStrategy = require("passport-local").Strategy;
 
 const User = require("./models/user");
-// register
+
 const register = async (name, password, done) => {
   const saltRounds = 10;
+
   try {
     if (!name) {
       throw new Error("A name was not provided");
     }
+
     const salt = await bcrypt.genSalt(saltRounds);
     const hash = await bcrypt.hash(password, salt);
     const user = await User.build({ name, passwordHash: hash });
+
     try {
       await user.save();
       done(null, user);
@@ -24,34 +27,36 @@ const register = async (name, password, done) => {
     done(error);
   }
 };
+
 const verify = async (token, done) => {
   try {
+    done(null, token.user);
   } catch (error) {
-    doone(null, token.user);
+    done(error);
   }
 };
-//login
+
 const login = async (username, password, done) => {
   try {
     const user = await User.findOne({ where: { name: username } });
 
     if (!user) {
-      return done(null, false, { msg: "incorrect username" });
+      return done(null, false, { msg: "Incorrect Username" });
     }
 
     const match = await bcrypt.compare(password, user.passwordHash);
     return match
       ? done(null, user)
-      : done(null, false, { msg: "incorrect password" });
+      : done(null, false, { msg: "Incorrect Password" });
   } catch (error) {
     done(error);
   }
 };
-//verify
+
 const verifyStrategy = new JWTStrategy(
   {
     secretOrKey: process.env.SECRET_KEY,
-    jwtFromRequest: ExctractJWT.fromUrlQueryParameter("secret_token"),
+    jwtFromRequest: ExtractJWT.fromUrlQueryParameter("secret_token"),
   },
   verify
 );
@@ -60,7 +65,9 @@ const registerStrategy = new LocalStrategy(
   { usernameField: "name", passwordField: "password" },
   register
 );
+
 const loginStrategy = new LocalStrategy(login);
+
 module.exports = {
   registerStrategy,
   verifyStrategy,
